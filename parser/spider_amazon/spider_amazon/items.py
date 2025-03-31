0f35023d985e0bdaedeb57b06938a1ba1f2b28d3
ellvalerie <117084699+ellvalerie@users.noreply.github.com>
2025-03-31 11:25:57 +0300

    the high almighty god society

--- parser/spider_amazon/spider_amazon/items.py
+++ parser/spider_amazon/spider_amazon/items.py
@@ -4,12 +4,22 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 
+
 import scrapy
 
 class SpiderAmazonItem(scrapy.Item):
-    # define the fields for your item here like:
-    product_name = scrapy.Field()
-    product_sale_price = scrapy.Field()
-    product_category = scrapy.Field()
-    product_original_price = scrapy.Field()
-    product_availability = scrapy.Field()
+    phone_full_name = scrapy.Field()
+    phone_list_price = scrapy.Field()
+    phone_sale_price = scrapy.Field()
+    phone_discount = scrapy.Field()
+    phone_review_rating = scrapy.Field()
+    phone_rate_number = scrapy.Field()
+    phone_brand = scrapy.Field()
+    phone_operating_system = scrapy.Field()
+    phone_ram_memory_size = scrapy.Field()
+    phone_memory_storage_capacity = scrapy.Field()
+    phone_screen_size = scrapy.Field()
+    phone_model_name = scrapy.Field()
+    phone_resolution = scrapy.Field()
+    phone_cell_tech = scrapy.Field()
+
--- parser/spider_amazon/spider_amazon/spiders/AmazonProductSpider.py
+++ parser/spider_amazon/spider_amazon/spiders/AmazonProductSpider.py
@@ -5,9 +5,10 @@ from spider_amazon.items import SpiderAmazonItem
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # Загружаем переменные из .env
+# подгрузка api-ключа для парсинга
+load_dotenv()  
 
-API = os.getenv('SCRAPER_API_KEY')  # Получаем ключ
+API = os.getenv('SCRAPER_API_KEY')  
 
 def get_url(url):
     payload = {'api_key': API, 'url': url}
@@ -17,31 +18,48 @@ def get_url(url):
 class AmazonproductspiderSpider(scrapy.Spider):
     name = 'AmazonProductSpider'
     allowed_domains = ['amazon.com', 'scraperapi.com']
-    start_urls = ['https://www.amazon.com/dp/B00JES3MO0', 'https://www.amazon.com/dp/B08NCC24HV',
-                  'https://www.amazon.com/dp/B09LCDF2VD', 'https://www.amazon.com/dp/B0B3D8Z7T8']
+    start_urls = ['https://www.amazon.com/s?k=phone&i=mobile&rh=n%3A2335752011%2Cp_123%3A110955%257C146762%257C254407%257C319106%257C338933%257C46655%257C473637%257C568349&dc&ds=v1%3AiN%2BjkmQH4ZujsIKhRuowoS%2BjOBKjFWcPI%2FpUaNkQv5Q&crid=VZIBEAHXCXRG&qid=1743404738&rnid=85457740011&sprefix=phone%2Caps%2C284&ref=sr_nr_p_123_13']
 
-    def start_requests(self): # придется переписать start_requests - функция, которая работает с GET-запросами
+    def start_requests(self):
         for url in self.start_urls:
-            yield scrapy.Request(url=get_url(url), callback=self.parse)
+            yield scrapy.Request(url=get_url(url), callback=self.parse_main_page)
 
-    # Как эта функция выглядит по дефолту
-        # def start_requests(self):
-        # if not self.start_urls and hasattr(self, 'start_url'):
-        #     raise AttributeError(
-        #         "Crawling could not start: 'start_urls' not found "
-        #         "or empty (but found 'start_url' attribute instead, "
-        #         "did you miss an 's'?)")
-        # for url in self.start_urls:
-        #     yield Request(url, dont_filter=True)
 
-    def parse(self, response):
+    def parse_main_page(self, response):
+        product_urls = response.xpath('//div[contains(@class, "a-section a-spacing-small")]//a[@class="a-link-normal s-line-clamp-4 s-link-style a-text-normal"]/@href').getall()
+        # product_urls = [link.get_attribute('href') for link in links]
+        base_url = "https://www.amazon.com"
+        full_urls = [base_url + url if url.startswith("/") else url for url in product_urls]
+        for url in full_urls:
+            yield scrapy.Request(url=get_url(url), callback=self.parse_phone)
+
+    def parse_phone(self, response):
         items = SpiderAmazonItem()
-        title = response.xpath('//h1[@id="title"]/span/text()').extract()
-        sale_price = response.xpath('//span[contains(@id,"swatch_price") or contains(@id,"saleprice")]/text()').extract()
-        category = response.xpath('//a[@class="a-link-normal a-color-tertiary"]/text()').extract()
-        availability = response.xpath('//div[@id="availability"]//text()').extract()
-        items['product_name'] = ''.join(title).strip()
-        items['product_sale_price'] = ''.join(sale_price).strip()
-        items['product_category'] = ','.join(map(lambda x: x.strip(), category)).strip()
-        items['product_availability'] = ''.join(availability).strip()
+        
+        items['phone_full_name'] = response.xpath('//h1[@id="title"]/span/text()').get(default='Не найдено').strip()
+        items['phone_review_rating'] = response.xpath('//span[@id="acrPopover"]/span[@class="a-declarative"]/a/span/text()').get(default='Не найдено').strip()
+        items['phone_rate_number'] = response.xpath('//span[@id="acrCustomerReviewText"]/text()').get(default='Не найдено').strip()
+        
+        price_whole = response.xpath('//span[@class="a-price-whole"]/text()').get()
+        price_fraction = response.xpath('//span[@class="a-price-fraction"]/text()').get()
+        if price_whole is None:
+            items['phone_sale_price'] = 'Не найдено'
+        else:
+            price_whole = price_whole.strip() if price_whole else ''
+            price_fraction = price_fraction.strip() if price_fraction else ''
+            items['phone_sale_price'] = price_whole + price_fraction
+
+        
+        items['phone_list_price'] = response.xpath('//span[@class="a-price a-text-price"]//span[@class="a-offscreen"]/text()').get(default='Не найдено').strip()
+        items['phone_discount'] = response.xpath('//span[contains(@class, "savingsPercentage")]/text()').get(default='Не найдено').strip()
+        
+        items['phone_brand'] = response.xpath('//tr[contains(@class, "po-brand")]/td[@class="a-span9"]/span/text()').get(default='Не найдено').strip()
+        items['phone_operating_system'] = response.xpath('//tr[contains(@class, "po-operating_system")]/td[@class="a-span9"]/span/text()').get(default='Не найдено').strip()
+        items['phone_ram_memory_size'] = response.xpath('//tr[contains(@class, "po-ram_memory.installed_size")]/td[@class="a-span9"]/span/text()').get(default='Не найдено').strip()
+        items['phone_memory_storage_capacity'] = response.xpath('//tr[contains(@class, "po-memory_storage_capacity")]/td[@class="a-span9"]/span/text()').get(default='Не найдено').strip()
+        items['phone_screen_size'] = response.xpath('//tr[contains(@class, "po-display.size")]/td[@class="a-span9"]/span/text()').get(default='Не найдено').strip()
+        items['phone_model_name'] = response.xpath('//tr[contains(@class, "po-model_name")]/td[@class="a-span9"]/span/text()').get(default='Не найдено').strip()
+        items['phone_resolution'] = response.xpath('//tr[contains(@class, "po-resolution")]/td[@class="a-span9"]/span/text()').get(default='Не найдено').strip()
+        items['phone_cell_tech'] = response.xpath('//tr[contains(@class, "po-cellular_technology")]/td[@class="a-span9"]/span/text()').get(default='Не найдено').strip()
+        
         yield items
