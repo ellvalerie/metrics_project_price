d1458a4d6f260b079de290c41491228f110e1145
ellvalerie <117084699+ellvalerie@users.noreply.github.com>
2025-04-04 20:49:57 +0300

    pupupu

--- parser/spider_amazon/spider_amazon/items.py
+++ parser/spider_amazon/spider_amazon/items.py
@@ -4,10 +4,13 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 
+# example of the phone page
+# https://www.amazon.com/Apple-iPhone-12-64GB-Black/dp/B08PP5MSVB/ref=sr_1_4?crid=VZIBEAHXCXRG&dib=eyJ2IjoiMSJ9.v8-v_dYf_IhKGWbz-_aPp3-4XK2XRl11XHJjXy4WtEts9hsMpTtHPgwUXySFmwvpaGMdnRdIRhJv6rDOLU6vLvRpH-3oHG7Xuw7T0oFH5Qv8GmUVMIidwogAc_UVfIUnisd_a4dI7yZJsAw3X8w7kMJ3AilEeuVsYrMZRld0QU__BaSazppisql7jNYALwkJE1QMfswxnNkg5u-UHSI_MP84yQkVnqCo0YzLWcOk-fiSa-qw8pB5zBtuOsM0uJKPo1O5lNW9hjplbMxvWPBV64noJlFktO24pbp2RdXrstA.BQKoPe8jejsAwTEiUi_yQRR-OZXT7woz1LI0lf0Fn8A&dib_tag=se&keywords=phone&qid=1743759026&refinements=p_123%3A110955%7C146762%7C254407%7C319106%7C338933%7C46655%7C473637%7C568349&rnid=85457740011&s=wireless&sprefix=phone%2Caps%2C284&sr=1-4&th=1
 
 import scrapy
 
 class SpiderAmazonItem(scrapy.Item):
+    # main info
     phone_full_name = scrapy.Field()
     phone_list_price = scrapy.Field()
     phone_sale_price = scrapy.Field()
@@ -15,11 +18,37 @@ class SpiderAmazonItem(scrapy.Item):
     phone_review_rating = scrapy.Field()
     phone_rate_number = scrapy.Field()
     phone_brand = scrapy.Field()
-    phone_operating_system = scrapy.Field()
-    phone_ram_memory_size = scrapy.Field()
-    phone_memory_storage_capacity = scrapy.Field()
-    phone_screen_size = scrapy.Field()
-    phone_model_name = scrapy.Field()
-    phone_resolution = scrapy.Field()
-    phone_cell_tech = scrapy.Field()
+    # other tech info
+    phone_dim = scrapy.Field()
+    phone_weight = scrapy.Field()
+    phone_ASIN = scrapy.Field() # уникальный идетификатор каждого товара на амазон
+    phone_model_numer = scrapy.Field()
+    phone_batteries = scrapy.Field()
+    phone_os = scrapy.Field()
+    phone_ram = scrapy.Field()
+    phone_wire_com_tech = scrapy.Field() # Wireless communication technologies
+    phone_conn_tech = scrapy.Field() # Connectivity technologies
+    phone_is_GPS = scrapy.Field()
+    phone_special_features = scrapy.Field()
+    phone_display_tech = scrapy.Field()
+    phone_other_display_features = scrapy.Field()
+    phone_human_int_input = scrapy.Field() # Human Interface Input
+    phone_scanner_res = scrapy.Field() # Scanner Resolution
+    phone_other_cam_features = scrapy.Field() # Other camera features
+    phone_from_factor = scrapy.Field()
+    phone_color = scrapy.Field()
+    phone_batt_power_rating = scrapy.Field() # Battery Power Rating
+    phone_set = scrapy.Field() # Whats in the box
+    phone_manufacturer = scrapy.Field() 
+    phone_date = scrapy.Field() # Date First Available
+    phone_mem_storage_cap = scrapy.Field() # Memory Storage Capacity
+    phone_standing_screen_display_size = scrapy.Field() # Standing screen display size
+    phone_ram_memory_installed_size = scrapy.Field()
+    # reviews 
+    phone_cust_rev_5_perc = scrapy.Field()
+    phone_cust_rev_4_perc = scrapy.Field()
+    phone_cust_rev_3_perc = scrapy.Field()
+    phone_cust_rev_2_perc = scrapy.Field()
+    phone_cust_rev_1_perc = scrapy.Field()
+    phone_cust_say = scrapy.Field() # dict {feature : {pos_rev : "num", neg_rev : "num", tone : "POSITIVE"/"NEGATIVE"/"MIXED"}
 
--- parser/spider_amazon/spider_amazon/spiders/AmazonProductSpider.py
+++ parser/spider_amazon/spider_amazon/spiders/AmazonProductSpider.py
@@ -18,11 +18,13 @@ def get_url(url):
 class AmazonproductspiderSpider(scrapy.Spider):
     name = 'AmazonProductSpider'
     allowed_domains = ['amazon.com', 'scraperapi.com']
-    start_urls = ['https://www.amazon.com/s?k=phone&i=mobile&rh=n%3A2335752011%2Cp_123%3A110955%257C146762%257C254407%257C319106%257C338933%257C46655%257C473637%257C568349&dc&ds=v1%3AiN%2BjkmQH4ZujsIKhRuowoS%2BjOBKjFWcPI%2FpUaNkQv5Q&crid=VZIBEAHXCXRG&qid=1743404738&rnid=85457740011&sprefix=phone%2Caps%2C284&ref=sr_nr_p_123_13']
-
+    # new 
+    # start_urls = [f'https://www.amazon.com/s?k=phone&i=mobile&rh=n%3A2335752011%2Cp_123%3A110955%257C146762%257C237204%257C254407%257C319106%257C329744%257C338933%257C358874%257C46655%257C473637%257C568349%2Cp_n_condition-type%3A6503240011&dc&page={i}&crid=VZIBEAHXCXRG&qid=1743764339&rnid=6503239011&sprefix=phone%2Caps%2C284&ref=sr_nr_p_n_condition-type_2&ds=v1%3AIjU2c0aUxE7Fr3xd9RvYh7NTIfZ3Ow3sQTENlKK2Ook' for i in range(1, 57)]
+    # renewed. Их нужно переключать и определять файл куда они записываются в команде scrapy.crawl
+    start_urls = [f'https://www.amazon.com/s?k=phone&i=mobile&rh=n%3A2335752011%2Cp_123%3A110955%257C146762%257C237204%257C254407%257C319106%257C329744%257C338933%257C358874%257C46655%257C473637%257C568349%2Cp_n_condition-type%3A16907722011&dc&page={i}&crid=VZIBEAHXCXRG&qid=1743771507&rnid=6503239011&sprefix=phone%2Caps%2C284&ref=sr_nr_p_n_condition-type_1&ds=v1%3AbQUafN4x2egXTwTGw1Kzw502jYnIsrq3cz2wQ1UYfqQ' for i in range(1, 3)]
     def start_requests(self):
         for url in self.start_urls:
-            yield scrapy.Request(url=get_url(url), callback=self.parse_main_page)
+            yield scrapy.Request(url= get_url(url), callback=self.parse_main_page)
 
 
     def parse_main_page(self, response):
@@ -31,35 +33,107 @@ class AmazonproductspiderSpider(scrapy.Spider):
         base_url = "https://www.amazon.com"
         full_urls = [base_url + url if url.startswith("/") else url for url in product_urls]
         for url in full_urls:
-            yield scrapy.Request(url=get_url(url), callback=self.parse_phone)
+            yield scrapy.Request(url = get_url(url), callback=self.parse_phone)
+
+    def get_tech_feature(self, response, field_name):
+        return response.xpath(f'//th[contains(text(), "{field_name}")]/following-sibling::td//text()').get(default='Sorry bro').strip()
+
+
 
     def parse_phone(self, response):
-        items = SpiderAmazonItem()
+        item = SpiderAmazonItem()
         
-        items['phone_full_name'] = response.xpath('//h1[@id="title"]/span/text()').get(default='Не найдено').strip()
-        items['phone_review_rating'] = response.xpath('//span[@id="acrPopover"]/span[@class="a-declarative"]/a/span/text()').get(default='Не найдено').strip()
-        items['phone_rate_number'] = response.xpath('//span[@id="acrCustomerReviewText"]/text()').get(default='Не найдено').strip()
+        item['phone_full_name'] = response.xpath('//h1[@id="title"]/span/text()').get(default='Sorry bro').strip()
+        item['phone_review_rating'] = response.xpath('//span[@id="acrPopover"]/span[@class="a-declarative"]/a/span/text()').get(default='Sorry bro').strip()
+        item['phone_rate_number'] = response.xpath('//span[@id="acrCustomerReviewText"]/text()').get(default='Sorry bro').strip()
         
-        price_whole = response.xpath('//span[@class="a-price-whole"]/text()').get()
-        price_fraction = response.xpath('//span[@class="a-price-fraction"]/text()').get()
+        price_blocks = response.xpath('//span[contains(@class, "priceToPay")]')
+        if not price_blocks:
+            self.logger.warning(f"No price block found at {response.url}")
+            item['phone_sale_price'] = 'Not available'
+            yield item
+            return
+
+        price_block = price_blocks[0]
+        price_whole = price_block.xpath('.//span[@class="a-price-whole"]/text()').get()
+        price_fraction = price_block.xpath('.//span[@class="a-price-fraction"]/text()').get()
         if price_whole is None:
-            items['phone_sale_price'] = 'Не найдено'
+            item['phone_sale_price'] = 'Sorry bro'
         else:
             price_whole = price_whole.strip() if price_whole else ''
             price_fraction = price_fraction.strip() if price_fraction else ''
-            items['phone_sale_price'] = price_whole + price_fraction
+            item['phone_sale_price'] = '$' + price_whole + '.' + price_fraction
+        
+        item['phone_list_price'] = response.xpath('//span[@class="a-price a-text-price"]//span[@class="a-offscreen"]/text()').get(default='Sorry bro').strip()
+        item['phone_discount'] = str(response.xpath(
+                                                '//span[contains(@class, "savingsPercentage")]/text() | '
+                                                '//span[contains(@class, "savingsPercentage")]/preceding::span[contains(@class, "aok-offscreen")][1]/text()'
+                                                ).re_first(r'(\d+)%') or '0') + '%'
+        item['phone_brand'] = response.xpath('//tr[contains(@class, "po-brand")]/td[@class="a-span9"]/span/text()').get(default='Sorry bro').strip()
 
+        # other tech info
+        item['phone_dim'] = self.get_tech_feature(response, 'Product Dimensions')
+        item['phone_weight'] = self.get_tech_feature(response, 'Item Weight')
+        item['phone_ASIN'] = self.get_tech_feature(response, 'ASIN')
+        item['phone_model_numer'] = self.get_tech_feature(response, 'Item model number')
+        item['phone_batteries'] = self.get_tech_feature(response, 'Batteries')
+        item['phone_os'] = self.get_tech_feature(response, 'OS')
+        item['phone_ram'] = self.get_tech_feature(response, 'RAM')
+        item['phone_wire_com_tech'] = self.get_tech_feature(response, 'Wireless communication technologies')
+        item['phone_conn_tech'] = self.get_tech_feature(response, 'Connectivity technologies')
+        item['phone_is_GPS'] = self.get_tech_feature(response, 'GPS')
+        item['phone_special_features'] = self.get_tech_feature(response, 'Special features')
+        item['phone_display_tech'] = self.get_tech_feature(response, 'Display technology')
+        item['phone_other_display_features'] = self.get_tech_feature(response, 'Other display features')
+        item['phone_human_int_input'] = self.get_tech_feature(response, 'Human Interface Input')
+        item['phone_scanner_res'] = self.get_tech_feature(response, 'Scanner Resolution')
+        item['phone_other_cam_features'] = self.get_tech_feature(response, 'Other camera features')
+        item['phone_from_factor'] = self.get_tech_feature(response, 'Form Factor')
+        item['phone_color'] = self.get_tech_feature(response, 'Color')
+        item['phone_batt_power_rating'] = self.get_tech_feature(response, 'Battery Power Rating')
+        item['phone_set'] = self.get_tech_feature(response, "Whats in the box")
+        item['phone_manufacturer'] = self.get_tech_feature(response, 'Manufacturer')
+        item['phone_date'] = self.get_tech_feature(response, 'Date First Available')
+        item['phone_mem_storage_cap'] = self.get_tech_feature(response, 'Memory Storage Capacity')
+        item['phone_standing_screen_display_size'] = self.get_tech_feature(response, 'Standing screen display size')
+        item['phone_ram_memory_installed_size'] = self.get_tech_feature(response, 'Ram Memory Installed Size')
+
+
+        # reviews
+        for i in range(5, 0, -1):
+            percent = response.xpath(f'//a[contains(@aria-label, "{i} percent of reviews")]/div[last()]/text()').get(default='Sorry bro')
+            item[f'phone_cust_rev_{i}_perc'] = percent
+
+            phone_cust_say = {}
+    
+        # tags
+        aspects = response.xpath('//div[@aria-label="Commonly Mentioned Aspects"]/a')
         
-        items['phone_list_price'] = response.xpath('//span[@class="a-price a-text-price"]//span[@class="a-offscreen"]/text()').get(default='Не найдено').strip()
-        items['phone_discount'] = response.xpath('//span[contains(@class, "savingsPercentage")]/text()').get(default='Не найдено').strip()
+        for aspect in aspects:
+            aspect_name = aspect.xpath('./text()').get(default='Sorry bro').strip()
+            
+            data_item = aspect.xpath('./@data-csa-c-item-id').get(default='Sorry bro')
+            tone = data_item.split('_')[-1] if data_item else 'UNKNOWN'
+            
+            bottom_sheet = response.xpath(
+                f'//div[@data-aspect="{aspect_name}" '
+                'or contains(@data-aspect, substring-before(@data-aspect, " "))]'
+            )
+            
+            pos_rev = bottom_sheet.xpath(
+                './/span[contains(@class, "text-positive")]/text()'
+            ).re_first(r'\d+') or '0'
+            
+            neg_rev = bottom_sheet.xpath(
+                './/span[contains(@class, "text-negative")]/text()'
+            ).re_first(r'\d+') or '0'
+            
+            phone_cust_say[aspect_name] = {
+                'pos_rev': pos_rev,
+                'neg_rev': neg_rev,
+                'tone': tone
+            }
         
-        items['phone_brand'] = response.xpath('//tr[contains(@class, "po-brand")]/td[@class="a-span9"]/span/text()').get(default='Не найдено').strip()
-        items['phone_operating_system'] = response.xpath('//tr[contains(@class, "po-operating_system")]/td[@class="a-span9"]/span/text()').get(default='Не найдено').strip()
-        items['phone_ram_memory_size'] = response.xpath('//tr[contains(@class, "po-ram_memory.installed_size")]/td[@class="a-span9"]/span/text()').get(default='Не найдено').strip()
-        items['phone_memory_storage_capacity'] = response.xpath('//tr[contains(@class, "po-memory_storage_capacity")]/td[@class="a-span9"]/span/text()').get(default='Не найдено').strip()
-        items['phone_screen_size'] = response.xpath('//tr[contains(@class, "po-display.size")]/td[@class="a-span9"]/span/text()').get(default='Не найдено').strip()
-        items['phone_model_name'] = response.xpath('//tr[contains(@class, "po-model_name")]/td[@class="a-span9"]/span/text()').get(default='Не найдено').strip()
-        items['phone_resolution'] = response.xpath('//tr[contains(@class, "po-resolution")]/td[@class="a-span9"]/span/text()').get(default='Не найдено').strip()
-        items['phone_cell_tech'] = response.xpath('//tr[contains(@class, "po-cellular_technology")]/td[@class="a-span9"]/span/text()').get(default='Не найдено').strip()
-        
-        yield items
+        item['phone_cust_say'] = phone_cust_say
+
+        yield item
